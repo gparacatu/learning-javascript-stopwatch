aa0b8a91f080ef19cfe91032b202e5600a95d3e2
Guilherme Paracatu <76759314+gparacatu@users.noreply.github.com>
2021-03-03 02:07:30 -0300

    Script 50%
    Scrip with 50%

---
+++ assets/scripts/script.js
@@ -0,0 +1,32 @@
+let timer;
+
+function cronometro() {
+
+    let segundosHTML = document.getElementById("segundos");
+    let minutosHTML = document.getElementById("minutos");
+    let minutosHorasHTML = document.getElementById("minutos-horas");
+    let horasHTML = document.getElementById("horas");
+
+    if (parseInt(segundosHTML.innerHTML) < 59) {
+        segundosHTML.innerHTML = parseInt(segundosHTML.innerHTML) + 1;
+    } else {
+        segundosHTML.innerHTML = 0;
+        minutosHTML.innerHTML = parseInt(minutosHTML.innerHTML) + 1;
+        minutosHorasHTML.innerHTML = parseInt(minutosHorasHTML.innerHTML) + 1;
+        if (parseInt(minutosHorasHTML.innerHTML) === 60) {
+            minutosHorasHTML.innerHTML = 0;
+            horasHTML.innerHTML = parseInt(horasHTML.innerHTML) + 1;
+        }
+    }
+
+}
+
+function start() {
+    timer = setInterval(cronometro, 1000);
+}
+
+function stop() {
+    clearInterval(timer);
+}
+
+ 
